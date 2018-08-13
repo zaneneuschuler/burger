7c4553a91d7fa2f0eb489880f4ac0082388cf91c
zane <neuschuler@gmail.com>
2018-08-13 11:51:42 -0700

    initial files, i suppose.

---
+++ config/orm.js
@@ -0,0 +1,93 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+//because i'm copy/pasting the orm i'm gonna comment it to show i know what's going on
+
+var orm = {
+    selectAll: function (tableInput, cb) {
+        //tableinput: which table to select from, cb is callback always
+        //create query string with a hardcoded string + tableinput
+        //keeps the sqlinjection away
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            //callback result (won't keep commenting this)
+            cb(result);
+        });
+    },
+    insertOne: function (table, cols, vals, cb) {
+        //table is table to insert into of course
+        //changed from cat orm: don't need cols or values to be arrays
+        //because by default burgs will not be eaten
+        var queryString = "INSERT INTO " + table;
+        queryString += " (";
+        queryString += cols;
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    // An example of objColVals would be {name: panther, sleepy: true}
+    updateOne: function (table, objColVals, condition, cb) {
+        //condition is burger id, objcolvals is what to change
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
+
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+          return arr.toString();
+    }
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT into burgers (burger_name) VALUES ("Cheesedborger"), ("Beefed Burger"), ("Da Big Burg"), ("The McNothing");
+
