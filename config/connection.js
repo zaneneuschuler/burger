8fa34502c2f105d9143e7c4b98617afbcc4ca4f8
zane <neuschuler@gmail.com>
2018-08-16 13:50:00 -0700

    Why am I so bad at this?

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,17 @@
 var mysql = require("mysql");
 
 var connection;
-if(process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
-}else{
-        connection = mysql.createConnection({
-            host: "localhost",
-            port: 3306,
-            user: "root",
-            password: "ducks",
-            database: "burgers_db"
-        });
-    }
-}
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "ducks",
+        database: "burgers_db"
+    });
+};
 
 // Make connection.
 connection.connect(function (err) {
